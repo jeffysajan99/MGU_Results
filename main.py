2788a35d3bfa2762a6be80531699487a2140bf5d
T90 <sarathm09@gmail.com>
2015-08-08 12:04:05 +0530

    rset cse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import shutil
+import subprocess
 import urllib
 import os
 from BeautifulSoup import BeautifulSoup
@@ -35,38 +37,46 @@ class S8btech:
             open("pdf/" + str(prn) + ".pdf", "wb").write(data)
             self.pdfToHTML(prn)
 
+
     def pdfToHTML(self, prn):
         subs, mks = [], []
         name, reg, colg, branch, marks, gpa = "", "", "", "", {}, []
-        os.system("pdftohtml pdf/" + str(prn) + ".pdf html/" + str(prn))
-        bs = BeautifulSoup(open("html/" + str(prn) + "/page1.html", "r").read())
-        for div in bs.findAll("div"):
-            if div and all(x in div["style"] for x in ["top:136px", "left:144px;"]):
-                name = div.text.replace(": ", "")
-            elif div and all(x in div["style"] for x in ["left:144px;", "top:108px;"]):
-                colg = div.text.replace(": ", "")
-            elif div and all(x in div["style"] for x in ["left:144px;", "top:122px;"]):
-                branch = div.text.replace(": ", "")
-            elif div and all(x in div["style"] for x in ["left:144px;", "top:151px;"]):
-                reg = div.text.replace(": ", "")
+        p = subprocess.Popen("pdftohtml pdf/" + str(prn) + ".pdf tmp/" + str(prn), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+        if os.path.exists("tmp/" + str(prn)):
+            bs = BeautifulSoup(open("tmp/" + str(prn) + "/page1.html", "r").read())
+            for div in bs.findAll("div"):
+                if div and all(x in div["style"] for x in ["top:136px", "left:144px;"]):
+                    name = div.text.replace(": ", "")
+                elif div and all(x in div["style"] for x in ["left:144px;", "top:108px;"]):
+                    colg = div.text.replace(": ", "")
+                elif div and all(x in div["style"] for x in ["left:144px;", "top:122px;"]):
+                    branch = div.text.replace(": ", "")
+                elif div and all(x in div["style"] for x in ["left:144px;", "top:151px;"]):
+                    reg = div.text.replace(": ", "")
 
-        f2 = bs.findAll("span", id="f2")
-        for span in f2:
-            if len(span.text)>1 and str(span.text[0]).isdigit() and span.text[1] == " ":
-                subs.append(span.text[2:])
-            if re.match("[\-0-9]+ [\-0-9]+ [\-0-9]+", span.text):
-                mks.append(span.text.split(" "))
-            elif re.match("[\-0-9]+ -", span.text):
-                ind = f2.index(span)
-                mks.append((span.text + " " + f2[ind+1].text).split(" "))
+            f2 = bs.findAll("span", id="f2")
+            for span in f2:
+                if len(span.text)>1 and str(span.text[0]).isdigit() and span.text[1] == " ":
+                    subs.append(span.text[2:])
+                if re.match("[\-0-9]+ [\-0-9]+ [\-0-9]+", span.text):
+                    mks.append(span.text.split(" "))
+                elif re.match("[\-0-9]+ [\-0-9]+", span.text):
+                    ind = f2.index(span)
+                    mks.append((span.text + " " + f2[ind+1].text).split(" "))
 
-        for sub in subs:
-            marks[sub] = mks[subs.index(sub)]
+            # for sub in subs:
+            #     marks[sub] = mks[subs.index(sub)]
 
-        gpa.append(re.findall("SGPA : ([0-9\.]+)", bs.text)[0])
-        gpa.append(re.findall("CGPA : ([0-9\.]+)", bs.text)[0])
+            gpa.append(re.findall("SGPA : ([\-0-9\.]+)", bs.text)[0])
+            gpa.append(re.findall("CGPA : ([\-0-9\.]+)", bs.text)[0])
 
-        self.exportResults(name, reg, colg, branch, mks, gpa)
+            self.exportResults(name, reg, colg, branch, mks, gpa)
+        else:
+            l = open("finished.csv", "a")
+            l.write("Err, " + str(prn) + "\n")
+            l.close()
+            print "Err, " + str(prn)
 
 
     def exportResults(self, name, reg, colg, branch, marks, gpa):
@@ -78,9 +88,9 @@ class S8btech:
                     "Marks 1 Int, Marks 1 Ext, Marks 1 Tot, "
                     "Marks 2 Int, Marks 2 Ext, Marks 2 Tot, "
                     "Marks 3 Int, Marks 3 Ext, Marks 3 Tot, "
-                    "Marks 4 Int, Marks 4 Ext, Marks 4 Tot, "
                     "Elective 1 Int, Elective 1 Ext, Elective 1 Tot, "
                     "Elective 2 Int, Elective 2 Ext, Elective 2 Tot, "
+                    "Lab Int, Lab Ext, Lab Tot, "
                     "Project Int, Project Ext, Project Tot, "
                     "Viva Voce Int, Viva Voce Ext, Viva Voce Tot, "
                     "SGPA, CGPA\n")
@@ -88,10 +98,16 @@ class S8btech:
         data = [reg, name, colg, branch, mk, gpa[0], gpa[1]]
         f.write(", ".join(data) + "\n")
         f.close()
+        l = open("finished.csv", "a")
+        l.write(reg + ", " + name + "\n")
+        l.close()
         print reg, name
+        shutil.rmtree("tmp/" + reg)
 
-
-S8btech().get(start=11012367, end=11012367)
+if __name__ == '__main__':
+    start = input("Starting Reg Number: ")
+    end = input("Ending Reg Number: ")
+    S8btech().get(start, end)
 
 
 
