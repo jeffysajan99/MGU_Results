515eb7a9dc5f9d968b0edb391f288b123c3fbb52
T90 <sarathm09@gmail.com>
2015-08-11 00:36:49 +0530

    ranklist calculator added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class S8btech:
     url = 'http://projects.mgu.ac.in/bTech/btechresult/index.php?' \
           'module=public&attrib=result&page=result&exam=37&Submit2=Submit&prn='
     start, end, filename = 0, 0, "result.csv"
+    rankData = []
 
 
     def __init__(self):
@@ -33,6 +34,7 @@ class S8btech:
             data = urllib.urlopen(url).read()
             open("pdf/" + str(prn) + ".pdf", "wb").write(data)
             self.pdfToHTML(prn)
+        self.publishRankList()
 
 
     def pdfToHTML(self, prn):
@@ -78,17 +80,10 @@ class S8btech:
             f = open(self.filename, "a")
         else:
             f = open(self.filename, "w")
-            f.write("RegNum, Name, College, Branch, "
-                    "Subject, Marks 1 Int, Marks 1 Ext, Marks 1 Tot, "
-                    "Subject, Marks 2 Int, Marks 2 Ext, Marks 2 Tot, "
-                    "Subject, Marks 3 Int, Marks 3 Ext, Marks 3 Tot, "
-                    "Subject, Elective 1 Int, Elective 1 Ext, Elective 1 Tot, "
-                    "Subject, Elective 2 Int, Elective 2 Ext, Elective 2 Tot, "
-                    "Subject, Lab Int, Lab Ext, Lab Tot, "
-                    "Project Int, Project Ext, Project Tot, "
-                    "Viva Voce Int, Viva Voce Ext, Viva Voce Tot, "
+            f.write("RegNum, Name, College, Branch, " +
+                    "Subject, Internal, External, Total, " * 8 +
                     "SGPA, CGPA\n")
-        for s in range(6):
+        for s in range(len(subs)):
             marks[s].insert(0, subs[s])
         mk = ", ".join([", ".join(i) for i in marks])
         data = [reg, name, colg, branch, mk, gpa[0], gpa[1]]
@@ -98,7 +93,70 @@ class S8btech:
         l.write(reg + ", " + name + "\n")
         l.close()
         print reg, name
+        self.rankData.append(data[0:4] + [str(i).strip() for i in data[4].split(",")] + data[5:])
         shutil.rmtree("tmp/" + reg)
+
+    def publishRankList(self):
+        displayColumns = [0, 1]
+        sortColumns = [7, 11, 15, 27, 31, 35]
+        Electives = [19, 23]
+        GPA = [37, 36]
+
+        elect_names = [[], []]
+
+        rankFile = open(self.filename.split(".")[0] + "_rankList.csv", "w")
+
+        data = []
+        for raw in self.rankData:
+            raw = [int(i) if i.isdigit() else i for i in raw]
+            data.append([0 if i == '-' else i for i in raw])
+            for el in Electives:
+                if raw[el-3] not in elect_names[Electives.index(el)]:
+                    elect_names[Electives.index(el)].append(raw[el-3])
+
+        # Complete Toppers
+        title = "B.Tech Ranklist,,,\n"
+        rankFile.write(title)
+        data.sort(key=lambda x: x[GPA[0]], reverse=True)
+        rankFile.write("\n".join([", ".join([str(data.index(stud)+1)] +
+                                            [str(stud[j]) for j in displayColumns+[GPA[0]]]) for stud in data]))
+        rankFile.write("\n")
+        rankFile.write("\n")
+        title = "S8 Ranklist,,,\n"
+        rankFile.write(title)
+        data.sort(key=lambda x: x[GPA[1]], reverse=True)
+        rankFile.write("\n".join([", ".join([str(data.index(stud)+1)] +
+                                            [str(stud[j]) for j in displayColumns+[GPA[1]]]) for stud in data]))
+        rankFile.write("\n")
+        rankFile.write("\n")
+
+        # Subject Toppers
+        for i in sortColumns:
+            title = raw[i-3] + " Rank,,,\n"
+            rankFile.write(title)
+            data.sort(key=lambda x: x[i], reverse=True)
+            rankFile.write("\n".join([", ".join([str(data.index(stud)+1)] +
+                                                [str(stud[j]) for j in displayColumns+[i]]) for stud in data]))
+            rankFile.write("\n")
+            rankFile.write("\n")
+
+        #  Elective Toppers
+        for i in range(2):
+            for el in elect_names[i]:
+                title = el + " Rank,,,\n"
+                rankFile.write(title)
+                temp = [s for s in data if s[Electives[i]-3] == el]
+                temp.sort(key=lambda x: x[Electives[i]], reverse=True)
+                rankFile.write("\n".join([", ".join([str(temp.index(stud)+1)] +
+                                                    [str(stud[j])
+                                                     for j in displayColumns+[Electives[i]]]) for stud in temp]))
+                rankFile.write("\n")
+                rankFile.write("\n")
+
+
+        rankFile.close()
+
+
 
 if __name__ == '__main__':
     start = input("Starting Reg Number: ")
--- test.py
+++ test.py
@@ -1,14 +1,23 @@
-a = ["aaa", "bbb", "ccc"]
-b = [
-    ["1", "1", "1"],
-    ["2", "2", "2"],
-    ["3", "3", "3"]
-]
-c = zip(a, b)
-print c
-for i in range(len(b)):
-    b[i].insert(0, a[i])
-
-print b
+# a = ["aaa", "bbb", "ccc"]
+# b = [
+#     ["1", "1", "1"],
+#     ["2", "2", "2"],
+#     ["3", "3", "3"]
+# ]
+# c = zip(a, b)
+# print c
+# for i in range(len(b)):
+#     b[i].insert(0, a[i])
+#
+# print b
 
 # print ", ".join(zip(a, [", ".join(i) for i in b]))
+
+data = [u'11012368', u'SAYOOJ K. C.', u'RAJAGIRI SCHOOL OF ENGINEERING AND TECHNOLOGY', u'COMPUTER SCIENCE AND ENGINEERING', u'High Performance Computing, 32, 30, -, Artificial Intelligence, 32, 27, -, Security in Computing, 31, 53, 84, E-commerce, 35, 53, 88, Advanced Mathematics, 38, 77, 115, Computer Graphics Lab, 35, 40, 75, Project, 72, -, 72, Viva Voce, -, 42, 42', u'-', u'-']
+
+raw = data[0:4] + [str(i).strip() for i in data[4].split(",")] + data[5:]
+raw = [int(i) if i.isdigit() else i for i in raw]
+data = [0 if i=='-' else i for i in raw]
+for i in data:
+    print data.index(i), i
+
